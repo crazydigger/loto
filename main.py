f4762c1e09afb2ba6f122810e97a53a7310140d9
borovirp <40pobGsb>
2022-01-26 11:02:54 +0300

    new update

--- main.py
+++ main.py
@@ -1,22 +1,36 @@
 import random
+
+
 class Player:
 
     def __init__(self, cells, n):
-        #карточка игрока
+        # карточка игрока
 
         self.card = []
-        #имя игрока
+        # имя игрока
         self.name = n
         for j in range(3):
             for i in range(cells):
-                self.card.append(str(random.randint(0,100)))
+                self.card.append(str(random.randint(0, 100)))
+
+    def close(self, num):
+        card_temp = []
+        for c in self.card:
+            if c == str(num):
+                card_temp.append('--')
+            else:
+                card_temp.append(c)
+        self.card = card_temp
+
     def show(self):
         print('игрок:', self.name)
         for j in range(3):
-            line=''
+            line = ''
             for c in self.card:
-                line=line+'!'+c+'!'
+                line = line + '!' + c + '!'
             print(line)
+
+
 # число игроков
 num = input('введите число игроков:')
 while int(num) <= 2:
@@ -25,8 +39,11 @@ while int(num) <= 2:
     players = []
     for i in range(int(num)):
         name = input('введите имя игрока' + str(i) + 'или <comp>если играет компьютер')
-        p = Player(5,name)
+        p = Player(5, name)
         players.append((p))
+        p.show()
+        num = input('какую цифру закрыть?')
+        p.close(int(num))
         p.show()
     if name == 'comp':
         print(name)
