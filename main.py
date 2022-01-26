c4e92e9793644b943efcf388b900fedd298acef4
borovirp <40pobGsb>
2022-01-26 10:51:09 +0300

    new update

--- main.py
+++ main.py
@@ -7,13 +7,16 @@ class Player:
         self.card = []
         #имя игрока
         self.name = n
-        for i in range(cells):
-            self.card.append(random.randint(0,99))
+        for j in range(3):
+            for i in range(cells):
+                self.card.append(str(random.randint(0,100)))
     def show(self):
         print('игрок:', self.name)
-        for c in self.card:
-
-            print(c)
+        for j in range(3):
+            line=''
+            for c in self.card:
+                line=line+'!'+c+'!'
+            print(line)
 # число игроков
 num = input('введите число игроков:')
 while int(num) <= 2:
@@ -22,7 +25,7 @@ while int(num) <= 2:
     players = []
     for i in range(int(num)):
         name = input('введите имя игрока' + str(i) + 'или <comp>если играет компьютер')
-        p = Player(15,name)
+        p = Player(5,name)
         players.append((p))
         p.show()
     if name == 'comp':
